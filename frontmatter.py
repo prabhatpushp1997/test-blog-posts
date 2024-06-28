bbc62b2b074861b8d6a3e652a6dc5c3d52fc2d14
prabhat pushp <46195662+prabhatpushp@users.noreply.github.com>
2024-06-28 12:14:45 +0530

    updated

--- frontmatter.py
+++ frontmatter.py
@@ -1,3 +1,4 @@
+import math
 import os
 import json
 import yaml
@@ -23,9 +24,33 @@ def extract_frontmatter(mdx_file_path):
         return frontmatter_data
     else:
         return None
+    
+def calculate_reading_time(mdx_file_path):
+    words_per_minute = 150
+    
+    try:
+        with open(mdx_file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+            
+            # Removing frontmatter (if present)
+            if content.startswith('---'):
+                content = content.split('---', 2)[2]
+            
+            words = content.split()
+            num_words = len(words)
+            reading_time_minutes = num_words / words_per_minute
+            
+            return math.ceil(reading_time_minutes)  # Returning reading time rounded to 2 decimal places
+    except FileNotFoundError:
+        print(f"File not found: {mdx_file_path}")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+    
 
 def convert_all_mdx_to_json(mdx_directory, output_json_path):
-    frontmatter_collection = {}
+    frontmatter_collection = []
 
     # Traverse the directory to find all MDX files
     for root, _, files in os.walk(mdx_directory):
@@ -33,10 +58,15 @@ def convert_all_mdx_to_json(mdx_directory, output_json_path):
             if file.endswith('.mdx'):
                 file_path = os.path.join(root, file)
                 frontmatter_data = extract_frontmatter(file_path)
-                if frontmatter_data:
+                if frontmatter_data and frontmatter_data['published']:
                     # Use the file name (without extension) as the key
                     file_name = os.path.splitext(file)[0]
-                    frontmatter_collection[file_name] = frontmatter_data
+                    frontmatter_data['slug'] = file_name
+                    frontmatter_data['readingTime'] = calculate_reading_time(file_path)
+                    frontmatter_collection.append(frontmatter_data)
+                    
+    # sort the collection by date here
+    frontmatter_collection.sort(key=lambda x: x['date'], reverse=True)
 
     # Write the collection of frontmatter to a single JSON file
     with open(output_json_path, 'w', encoding='utf-8') as output_file:
